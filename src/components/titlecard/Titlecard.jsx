11093115c28318df8e993e52d7533c80c7a18956
nayakom16 <nayakankush71@gmail.com>
2025-05-18 21:05:35 +0530

    first commit

---
+++ src/components/titlecard/Titlecard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './titlecard.css'
+
+const Titlecard = () => {
+  return (
+    <div className='titlecard'>
+      
+    </div>
+  )
+}
+
+export default Titlecard
